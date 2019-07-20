201a8a9ff9bc1f04d71e67c170a4adaa8d136a70
viniciussanches22 <viniciussanches22@outlook.com>
2019-07-20 02:20:06 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Google.Apis.Auth;
+
+namespace GooglePhotosUploader
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Google.Apis.Oauth2.v2.Data.Tokeninfo
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
