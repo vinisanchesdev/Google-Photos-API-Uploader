c17b659c129cfca8d4262432a2d8768078518795
viniciussanches22 <viniciussanches22@outlook.com>
2019-07-20 12:09:29 -0300

    implement token generation

--- Program.cs
+++ Program.cs
@@ -1,15 +1,54 @@
-﻿using System;
-using Google.Apis.Auth;
-using Google.Apis.Oauth2.v2.Data;
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Docs.v1;
+using Google.Apis.Docs.v1.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
 
-namespace GooglePhotosUploader
+namespace DocsQuickstart
 {
-    public class Program
+    class Program
     {
+        // If modifying these scopes, delete your previously saved credentials
+        // at ~/.credentials/docs.googleapis.com-dotnet-quickstart.json
+        static string[] Scopes = { DocsService.Scope.DocumentsReadonly };
+        static string ApplicationName = "Google Docs API .NET Quickstart";
+
         static void Main(string[] args)
         {
-            var test = new Tokeninfo();
-            Console.WriteLine("Hello World!");
+            UserCredential credential;
+
+            using (var stream = new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
+            {
+                string credPath = "token.json";
+
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+            }
+
+            // Create Google Docs API service.
+            var service = new DocsService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+
+            // Define request parameters.
+            String documentId = "195j9eDD3ccgjQRttHhJPymLJUCOUjs-jmwTrekvdjFE";
+            DocumentsResource.GetRequest request = service.Documents.Get(documentId);
+
+            // Prints the title of the requested doc:
+            // https://docs.google.com/document/d/195j9eDD3ccgjQRttHhJPymLJUCOUjs-jmwTrekvdjFE/edit
+            Document doc = request.Execute();
+            Console.WriteLine("The title of the doc is: {0}", doc.Title);
         }
     }
 }
